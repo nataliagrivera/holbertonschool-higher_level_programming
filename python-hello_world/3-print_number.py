c9451f108db05d6c8252e3dadcbac50e4ce79e1b
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-25 21:09:40 -0400

    Adding task 1. Correcting the fact that i was casting variable number into the string

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery street") # Print a string with a variable
-# The variable number is an integer with a value of 98
+print(f"{number} Battery street")  # Print a string with a variable
