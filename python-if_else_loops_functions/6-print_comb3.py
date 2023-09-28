78cb464eabece3b5279f333a1d7b01c173ba8ff8
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-28 19:19:33 -0400

    Task 6. All possible combination of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i < 8:
-            print(f"{i:d}{j:d}, ", end="")
+            print("{:d}{:d}, ".format(i, j), end="")
         else:
-            print(f"{i:d}{j:d}\n", end="")
+            print("{:d}{:d}\n".format(i, j), end="")
