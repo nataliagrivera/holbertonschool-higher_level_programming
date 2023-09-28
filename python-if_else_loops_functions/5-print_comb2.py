0481784cb5da2103ed33f2f09cde8d9ea899126c
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-27 20:49:23 -0400

    Adding task 5. This program prints nnumbers from 0 to 90

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+output = ""
 for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+    output += f"{i:02d}, " if i < 99 else f"{i:02d}\n"
+
+print(output, end="")
