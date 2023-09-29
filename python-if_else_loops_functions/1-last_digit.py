ed9d5251b5cbbe3b1765d4c1fd7088631baa1c2f
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-29 18:25:40 -0400

    fixing first taskgi

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = -number
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_digit = number % -10
+else:
     last_digit = number % 10
-    print("{:d}".format(last_digit), end="")
-    return last_digit
+if last_digit > 5:
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
+else:
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
