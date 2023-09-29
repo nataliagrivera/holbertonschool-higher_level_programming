221146a06eac9c1ffa7c33b3e1d6ad168796be51
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-28 20:04:54 -0400

    program prints upper case

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{:c}".format(ord(str[i]) - 32), end="")
-        else:
-            print("{:c}".format(ord(str[i])), end="")
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
     print("")
