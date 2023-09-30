7a1101b76b429912ba1e1bd142753b787a564725
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-29 20:33:41 -0400

    Prints the number of and the list of aguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    argc = len(argv) - 1
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
-        print("1: {}".format(argv[1]))
-    else:
-        print("{} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, argv[i]))
+    import sys
+argc = len(sys.argv)
+
+if argc == 1:
+    print("0 arguments.")
+elif argc == 2:
+    print("1 argument:")
+    print("1: {}".format(str(sys.argv[1])))
+else:
+    print("{} arguments:".format(argc - 1))
+    for j in range(1, argc):
+        print("{}: {}".format(j, str(sys.argv[j])))
