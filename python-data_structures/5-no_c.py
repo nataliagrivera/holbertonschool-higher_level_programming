d9a882e83209eaac3b066a16940c85101ee5e7ec
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-03 19:51:20 -0400

    Corection of the fifth task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for i in my_string :
+    for i in my_string:
         if i == 'c' or i == 'C':
             new_string = my_string.replace(i,"")
+            return new_string
+        else:
+            new_string = my_string
             return new_string
         
