dfd72ee6b68e40f1c4b445e6c7ae9d6c278e26e7
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-03 16:56:06 -0400

    Adding third task which reverses a list

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in range(len(my_list)):
+            reversed_list = my_list[::-1] # reverses the list
+            print("{:d}".format(reversed_list[i]))
