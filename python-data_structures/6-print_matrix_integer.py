4eddf6875a07e2fd22f3e804da160150bb67d4df
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-03 20:15:03 -0400

    corrections on the pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
-            print("{:d}".format(column), end=" ") # end=" " prints a space instead of a new line))
-        print() # prints a new line
-
+            print("{:d}".format(column), end=" ")
+            print()  # prints a new line
