4240846a277c2a182bcecf2c2541a7facdcb02ba
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-05 16:22:11 -0400

    changing for loop in task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-        for key in sorted(a_dictionary.keys()):
-            print("{}: {} ".format(key, a_dictionary[key]))
+        for key in sorted(a_dictionary.items()):
+            print("{}: {} ".format(key[0], key[1]))
