a3492eedde85ce87b2fb669c9c6ed5cead254df7
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-05 14:28:04 -0400

    changed spacing in print

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary): #sorted function will sort the keys in the dictionary
-        print("{}:{}".format(key,a_dictionary[key])) #.format will print the key and the value associated
+        print("{}: {} ".format(key,a_dictionary[key])) #.format will print the key and the value associated
