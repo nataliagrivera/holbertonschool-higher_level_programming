f5ca9abdadf50f461144275154160c8f3efaa9c6
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-05 14:23:23 -0400

    this will sort the keys in the dictionary

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary): #sorted function will sort the keys in the dictionary
+        print("{}:{}".format(key,a_dictionary[key])) #.format will print the key and the value associated
