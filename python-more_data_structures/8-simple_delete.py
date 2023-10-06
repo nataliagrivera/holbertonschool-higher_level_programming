6ba3cf5c2600396820391abe44028bd7495463bc
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-06 10:15:15 -0400

    correcting spelling error in the signature

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def somple_delete(a_dictionary, keys=""):
+def simple_delete(a_dictionary, keys=""):
     if keys in a_dictionary:
         del a_dictionary[keys]
     return a_dictionary
