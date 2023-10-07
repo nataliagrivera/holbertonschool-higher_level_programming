4222b43a632afbbea2f767f8da34cd4fc0f65844
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-06 23:24:13 -0400

    fixing output

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-    except IndexError:
-        print()
-        return i
-    print()
-    return i + 1
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
