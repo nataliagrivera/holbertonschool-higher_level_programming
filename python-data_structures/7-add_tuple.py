10830597ecaf1483523ed2984a69ec61d50b0f53
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-06 23:26:52 -0400

    fixing only missing checkers

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)  # adds 0 to tuple_a
-    tuple_b = tuple_b + (0, 0)  # adds 0 to tuple_b
-    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])  # adds tuple_a and tuple_b
-    return tuple_c
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0, 0)
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
