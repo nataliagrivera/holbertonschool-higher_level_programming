f52d5a352f018cc1de4d8e8269f90a1078095fe4
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-06 23:16:30 -0400

    .

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    if idx == len(my_list) - 1:
-        return my_list[:-1]
-
-    new_list = my_list[:idx] + my_list[idx + 1:]
-    return new_list
+    if 0 <= idx < len(my_list):
+        return my_list[:idx] + my_list[idx + 1:]
+    return my_list
