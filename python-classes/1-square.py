f2f2d77f2c14aa04223188ff8e316f603f46e3b4
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-10 22:01:24 -0400

    first iteration of a private intance attribute: size

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module creates a class named square"""
+class Square:
+    def __init__(self, size):
+        self._size = size # private instance attribute
+
+my_square = Square(3)
