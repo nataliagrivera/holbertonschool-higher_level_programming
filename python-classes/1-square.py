8c672320a010d7a71ab1ec59f634127250286f51
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-10 22:06:22 -0400

    adding pycodestyle corrections

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module creates a class named square"""
 class Square:
+    """This class is empty"""
     def __init__(self, size):
+        """This is the instantiation method"""
         self._size = size # private instance attribute
