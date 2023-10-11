67a1c8b6e3fd727e9b9f7c88db4cb15a1b44b463
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-10 22:41:23 -0400

    adding pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-
 """This module creates a class named square"""
+
+
 class Square:
     """class Square that defines a square"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Instantiation"""
         self.__size = size
-        if type(size) is not int:  #check if size is not an integer
+        if type(size) is not int:  # check if size is not an integer
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
