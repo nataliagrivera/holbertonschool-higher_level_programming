5cc533854a4da09b6ad9310a514ad3909ab416ae
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-10 22:10:23 -0400

    correcting line 9

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module creates a class named square"""
+
+
 class Square:
     """This class is empty"""
     def __init__(self, size):
         """This is the instantiation method"""
-        self._size = size # private instance attribute
+        self._Square_size = size  # private instance attribute
