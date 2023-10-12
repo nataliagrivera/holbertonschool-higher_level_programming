1bd2f0abcca194b302201b3546f5db399461f62b
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-12 15:31:04 -0400

    Nan to integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,12 +5,9 @@
 def add_integer(a, b=98):
     """This function adds 2 integers"""
 
+    
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
-    if a != a:
-        raise ValueError("a cannot be NaN")
-    if b != b:
-        raise ValueError("b cannot be NaN")
     return int(a) + int(b)
