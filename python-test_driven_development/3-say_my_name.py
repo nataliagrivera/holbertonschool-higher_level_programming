208403274cf441fff82d4c0fd7a3c6944978e836
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-13 17:05:57 -0400

    correcting spelling error

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,5 +8,5 @@ def say_my_name(first_name, last_name=""):
     if first_name != str(first_name):
         raise TypeError('first_name must be a string')
     if last_name != str(last_name):
-        raise TypeError('las_name must be a string')
+        raise TypeError('last_name must be a string')
     print('My name is {}{}'.format(first_name, last_name))
