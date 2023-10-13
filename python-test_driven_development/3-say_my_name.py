c23fd6699fd7d36b714fea72359e5741bd68f33e
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-13 18:14:26 -0400

    fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""This module has a function that prints a square"""
+"""
+Module 3-say_my_name
+Contains method that prints out "My name is [full name]"
+Takes in two strings: first and last name
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """This function prints My name is"<first name> <last_name>"""
-
-    if type(first_name is not str):
-        raise TypeError('first_name must be a string')
-
-    if last_name is not str:
-        raise TypeError('last_name must be a string')
-
-    print('My name is {}{}'.format(first_name, last_name))
+    """
+    Prints "My name is [full name]"
+    """
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        print("My name is {:s} {:s}".format(first_name, last_name))
+    else:
+        raise TypeError("{:s} must be a string".
+                        format("first_name" if isinstance(last_name, str)
+                               else "last_name"))
