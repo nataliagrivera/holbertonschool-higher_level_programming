b1284946592e2998c5667b055267c320fb4176e3
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-13 17:23:43 -0400

    fixing name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,8 +5,10 @@
 def say_my_name(first_name, last_name=""):
     """This function prints My name is"<first name> <last_name>"""
 
-    if first_name != str(first_name):
+    if type(first_name is not str):
         raise TypeError('first_name must be a string')
-    if last_name != str(last_name):
+    
+    if last_name is not str:
         raise TypeError('last_name must be a string')
+    
     print('My name is {}{}'.format(first_name, last_name))
