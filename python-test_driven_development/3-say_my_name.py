5ff8dc752c0f974623a9388b1276ca7d163442ed
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-13 16:59:57 -0400

    adding function and test file

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module has a function that prints a square"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This function prints My name is"<first name> <last_name>"""
+
+    if first_name != str(first_name):
+        raise TypeError('first_name must be a string')
+    if last_name != str(last_name):
+        raise TypeError('las_name must be a string')
+    print('My name is {}{}'.format(first_name, last_name))
