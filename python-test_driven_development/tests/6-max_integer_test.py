8567e31ecab0659a35937089f072d1001a0d5d0d
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-13 17:59:07 -0400

    adding unittest

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,12 @@
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+def max_integer(list=[]):
+    """This function finds the biggest integer of a list."""
+    if len(list) == 0:
+        return None
+    max = list[0]
+    for i in range(len(list)):
+        if list[i] > max:
+            max = list[i]
+    return max
