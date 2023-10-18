e0a212f54c71e9b28041cd3e637330f372931e85
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-18 09:48:53 -0400

    adding spacing on task 0 to pass checkers

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains a function call
+lookup that returns a list of availables
+attributes and method of the pass object
+"""
+
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """
+    This function returns a list of availables
+    attributes and methos of an object
+    passed to the function.
+    """
     return dir(obj)
