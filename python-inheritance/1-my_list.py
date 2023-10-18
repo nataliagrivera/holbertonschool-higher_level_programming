d158744e34c13deebb12b2e822e7f1a65110c680
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-18 18:48:27 -0400

    fixing inheritance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-
+"""
+This module contains a child class
+named MyList that inherit from list.
+"""
 
 
 class MyList(list):
-    """This is a class MyList that inherits from list"""
-
-
+    """
+    This class inherts from list,
+    it containd also a function that print
+    a sorted list
+    """
     def print_sorted(self):
-        """This function prints the list, but sorted (ascending sort)"""
-        print(sorted(self))
+        """
+        This function prints a list sorted
+        in ascending order of integers
+        """
+        list_sorted = []
+        for num in self:
+            list_sorted.append(num)
+        list_sorted.sort()
+        print(list_sorted)
