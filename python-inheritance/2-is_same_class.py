b279f4ca6337dd47fea04c77b2d59b420dc25019
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-18 23:04:25 -0400

    adding syntax to comply with checkers

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,4 +6,7 @@ def is_same_class(obj, a_class):
     This function checks if 
     an object is exactly an instance
     """
-    return type(obj) is a_class
+    if type(obj) is a_class:
+        return True
+    else:
+        return False    
