81911df693592ab49b857255548a6a407eb03e62
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-20 11:25:07 -0400

    first try task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module has aa empty class named Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class namd Base Geometry
+    """
+    pass
+ 
+def area(self):
+    """
+    This function raises an exception
+    """
+    raise Exception ('area()is not implemented')
