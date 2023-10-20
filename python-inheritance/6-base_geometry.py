f95138278fbced87a24f79cd7420a29889a757aa
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-20 11:33:36 -0400

    why is it only getting worse

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,11 +5,11 @@ This module has aa empty class named Geometry
 
 
 class BaseGeometry:
-"""
- class exception
-"""
-def area(self):
-     """
+    """
+    class exception
+    """
+    def area(self):
+        """
         This function raises an exception
         """
         raise Exception ('area()is not implemented')
