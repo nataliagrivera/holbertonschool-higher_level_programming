144516d5853527a7793162a4f16e3af851d3943b
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-20 11:31:10 -0400

    did not remove pass and indentation needed fixing

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,13 +5,11 @@ This module has aa empty class named Geometry
 
 
 class BaseGeometry:
-    """
-    Empty class namd Base Geometry
-    """
-    pass
- 
+"""
+ class exception
+"""
 def area(self):
-    """
-    This function raises an exception
-    """
-    raise Exception ('area()is not implemented')
+     """
+        This function raises an exception
+        """
+        raise Exception ('area()is not implemented')
