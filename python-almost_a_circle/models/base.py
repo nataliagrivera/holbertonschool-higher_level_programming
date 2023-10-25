eeb5f458976031329e9fbde4a45e92456678713d
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-25 12:04:51 -0400

    more indentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,11 +6,11 @@ class Base:
     """Represents the base of all other classes in this project"""
     __nb_objects = 0
 
-def __init__(self, id=None):
-    """ Init method """
+    def __init__(self, id=None):
+        """ Init method """
 
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
