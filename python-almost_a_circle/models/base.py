48decfbe52158965b5fc79a6ad77db9f69352c71
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-25 11:38:25 -0400

    adding init and base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3 
+"""Class Base"""
+
+class Base:
+    """Represents the base of all other classes in this project"""
+__nb_objects = 0
+
+def __init__(self, id=None):
+    """ Init method """
+    if id is not None:
+        self.id = id
+    else:
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
