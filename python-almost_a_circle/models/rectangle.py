ea73bf83b3436eda43f60ae1ba1dc1a29bb2e34a
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-25 11:04:30 -0400

    trying for checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,81 +6,79 @@ from models.base import Base
 class Rectangle(Base):
     """Represents rectangle class"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initialie rectangle class
-        Args:
-            width (int): width of rectangle
-            height (int): height of rectangle
-            x (int): x coordinate of rectangle
-            y (int): y coordinate of rectangle
-            id (int): id of rectangle
-        Raises:
-            TypeError: if width, height, x, y, or id is not an int
-            ValueError: if width, height, x, y, or id is <= 0
-        """
+    def init(self, width, height, x=0, y=0, id=None):
+        """Initilizes the rectangle.
 
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+            x (int): x coordinate of the rectangle
+            y (int): y coordinate of the rectangle
+            id (int): id of the rectangle
+        Raises:
+            TypeError: If either of width or height is not an integer
+            ValueError: If either of width or height is <= 0
+            TypeError: If either of x or y is not an integer
+            ValueError: If either of x or y is < 0
+        """
+        super().init(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-        super().__init__(id)
+    @property
+    def width(self):
+        """
+        Getter method for width
+        """
+        return self.width
 
-        # Getter methods for width, height, x, and y
-        @property
-        def width(self):
-            """
-            Getter method for width
-            """
-            return self.__width
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for width
+        """
+        self.width = value
 
-        @width.setter
-        def width(self, value):
-            """
-            Setter method for width
-            """
-            self.__width = value
+    @property
+    def height(self):
+        """
+        Getter method for height
+        """
+        return self.height
 
-        @property
-        def height(self):
-            """
-            Getter method for height
-            """
-            return self.__height
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for height
+        """
+        self.height = value
 
-        @height.setter
-        def height(self, value):
-            """
-            Setter method for height
-            """
-            self.__height = value
+    @property
+    def x(self):
+        """
+        Getter method for x
+        """
+        return self.x
 
-        @property
-        def x(self):
-            """
-            Getter method for x
-            """
-            return self.__x
+    @x.setter
+    def x(self, value):
+        """
+        Setter method for x
+        """
+        self.x = value
 
-        @x.setter
-        def x(self, value):
-            """
-            Setter method for x
-            """
-            self.__x = value
+    @property
+    def y(self):
+        """
+        Getter method for y
+        """
+        return self.y
 
-        @property
-        def y(self):
-            """
-            Getter method for y
-            """
-            return self.__y
-
-        # Setter methods for width, height, x, and y
-
-        @y.setter
-        def y(self, value):
-            """
-            Setter method for y
-            """
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """
+        Setter method for y
+        """
+        self.y = value
