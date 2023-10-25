fa6d3483a8fad8a0017504a0d34d2b39427fe8ce
nataliagrivera <nataliagirvera62@gmail.com>
2023-10-25 12:07:36 -0400

    vamos a ver

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,81 +7,83 @@ class Rectangle(Base):
     """Represents rectangle class"""
 
 
-def __init__(self, width, height, x=0, y=0, id=None):
-    """Initilizes the rectangle.
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initilizes the rectangle.
 
-    Args:
-    width (int): width of the rectangle
-    height (int): height of the rectangle
-    x (int): x coordinate of the rectangle
-    y (int): y coordinate of the rectangle
-    id (int): id of the rectangle
+        Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+        x (int): x coordinate of the rectangle
+        y (int): y coordinate of the rectangle
+        id (int): id of the rectangle
 
-    Raises:
-    TypeError: If either of width or height is not an integer
-    ValueError: If either of width or height is <= 0
-    TypeError: If either of x or y is not an integer
-    ValueError: If either of x or y is < 0
+        Raises:
+        TypeError: If either of width or height is not an integer
+        ValueError: If either of width or height is <= 0
+        TypeError: If either of x or y is not an integer
+        ValueError: If either of x or y is < 0
 
-    """
-    super().__init__(id)
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
+        """
+        super().__init__(id)
 
-    @property
-    def width(self):
-        """
-        Getter method for width
-        """
-        return self.width
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-    @width.setter
-    def width(self, value):
-        """
-        Setter method for width
-        """
-        self.width = value
+        @property
+        def width(self):
+            """
+            Getter method for width
+            """
+            return self.width
 
-    @property
-    def height(self):
-        """
-        Getter method for height
-        """
-        return self.height
+        @width.setter
+        def width(self, value):
+            """
+            Setter method for width
+            """
+            self.width = value
 
-    @height.setter
-    def height(self, value):
-        """
-        Setter method for height
-        """
-        self.height = value
+        @property
+        def height(self):
+            """
+            Getter method for height
+            """
+            return self.height
 
-    @property
-    def x(self):
-        """
-        Getter method for x
-        """
-        return self.x
+        @height.setter
+        def height(self, value):
+            """
+            Setter method for height
+            """
+            self.height = value
 
-    @x.setter
-    def x(self, value):
-        """
-        Setter method for x
-        """
-        self.x = value
+        @property
+        def x(self):
+            """
+            Getter method for x
+            """
+            return self.x
 
-    @property
-    def y(self):
-        """
-        Getter method for y
-        """
-        return self.y
+        @x.setter
+        def x(self, value):
+            """
+            Setter method for x
+            """
+            self.x = value
 
-    @y.setter
-    def y(self, value):
-        """
-        Setter method for y
-        """
-        self.y = value
+        @property
+        def y(self):
+            """
+            Getter method for y
+            """
+            return self.y
+
+        @y.setter
+        def y(self, value):
+            """
+            Setter method for y
+            """
+            self.y = value
