853ccfd3a4321609a327d864d775e0fa0f33620c
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 00:20:04 -0400

    Update SQL query to show table creation statement

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- Prints the description of the table first_table from database hbtn_0c_0 in your MySQL server.
-USE information_schema;
-
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table;
