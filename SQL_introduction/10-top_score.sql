c94f7942a1e2817dbad5ba65776490c9c3ed86a0
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 01:14:11 -0400

    Refactor SQL query to include name column in result

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table with the highest score.
 
-SELECT * FROM second_table WHERE score = (SELECT MAX(score) FROM second_table);
+SELECT score, name FROM second_table WHERE score = (SELECT MAX(score) FROM second_table);
