0ff9b590d025a6f23a1dd3f21a74966782120aab
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 18:07:25 -0400

    Refactor SQL query to use HAVING clause for scores greater than or equal to 10

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Lists all rrecords with a score of 10 or less in second_table.
 
-SELECT score, name FROM second_table WHERE score <= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table HAVING score>=10 ORDER BY score DESC;
