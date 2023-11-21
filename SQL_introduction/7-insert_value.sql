822fb777240853b5e7608f3f92f1fa1c0419d743
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 00:30:54 -0400

    Fix SQL insert statement

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---Inserts a new row in the table first_table
-INSERT INTO first_table (89, "Best School");
+-- Inserts a new row in the table first_table
+INSERT INTO first_table values(89, "Best School");
