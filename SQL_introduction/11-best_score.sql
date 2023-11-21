192053861fee4fdbd6f7ad0e502d8ec95da57c2d
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 18:01:16 -0400

    Refactor SQL query to include name and order by score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Lists all rrecords with a score of 10 or less in second_table.
 
-SELECT FROM second_table WHERE score <= 10;
+SELECT score, name FROM second_table WHERE score <= 10 ORDER BY score DESC;
