a828028b7ec367eb2e4d75704d0e53a67b9abf30
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 01:06:07 -0400

    Add SQL script for creating and populating second_table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creates a table called seocnd_table in the database.
+CREATE TABLE IF NOT EXISTS second_table(INT, name VARCHAR(256), INT);
+
+--inserts a record into the table second_table.
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
