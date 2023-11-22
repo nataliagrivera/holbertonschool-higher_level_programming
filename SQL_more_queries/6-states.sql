24a552bbb95e099611c6ba0744cb237fcee7bebb
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:33:37 -0400

    Fix typo in SQL script

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
--- Cretaes a database base htbn_0d_usa and states in that database
-CREATE IF NOT EXISTS database htbn_0d_usa;
+-- Creates a database base htbn_0d_usa and states in that database
+CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
