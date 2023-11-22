19b649e334ea4e0824f7b001be81691097139b10
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:53:58 -0400

    Refactor id_not_null table to add NOT NULL constraint on name column

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- Creates a table "id_not_null" on your MySQL server.
+-- Creates id_not_null table 
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
-    name VARCHAR(256)
-)
+    name VARCHAR(256) NOT NULL
+);
