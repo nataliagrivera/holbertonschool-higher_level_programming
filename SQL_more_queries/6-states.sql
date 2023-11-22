8760d42bc373d4152929ff63e5deb1f65e438e6f
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:39:59 -0400

    Fix database and table names in SQL queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
--- Creates a database base htbn_0d_usa and states in that database
-CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
+-- Creates a database "hbtn_od_usa" and a table "states" (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     UNIQUE (id)
-);
+)
