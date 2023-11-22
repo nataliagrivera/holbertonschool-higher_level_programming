bab842b52b54d8e9006550436d386ce6ce15295d
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 21:02:27 -0400

    Update user_0d_1 credentials and privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create a server user.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
