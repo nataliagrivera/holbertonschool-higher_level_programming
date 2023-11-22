834e70cea7178611107f254bf3fafe13a9d22e00
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:35:35 -0400

    Refactor states table creation

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
--- Creates a database base htbn_0d_usa and states in that database
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
+USE htbn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
-    UNIQUE (id)
+    name VARCHAR(256) NOT NULL
 );
