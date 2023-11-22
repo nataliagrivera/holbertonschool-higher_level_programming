5206ec1966d9260798069b5649d8349db33a749a
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:32:28 -0400

    Add SQL script to create 'states' table in 'htbn_0d_usa' database

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Cretaes a database base htbn_0d_usa and states in that database
+CREATE IF NOT EXISTS database htbn_0d_usa;
+CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE (id)
+);
