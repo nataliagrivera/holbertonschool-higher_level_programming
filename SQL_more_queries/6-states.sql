278d3e51b35da4a5961d4cd32a4cbffee269983a
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:36:45 -0400

    Add unique constraint to states table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
+-- Creates a database base htbn_0d_usa and states in that database
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-USE htbn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
+    UNIQUE (id)
 );
