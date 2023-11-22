a9070cd69cb5ecb2241594f9b4bbc246e31ca69a
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-21 23:26:28 -0400

    Fix unique_id table definition

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Create unique_id on server. Int with default value of 1 and must be unique.
 CREATE TABLE IF NOT EXIST unique_id (
-id INT DEFAULT 1 UNIQUE,
+id INT DEFAULT 1,
     name VARCHAR(256)
-    UNIQUE(id)
+    UNIQUE (id)
 );
