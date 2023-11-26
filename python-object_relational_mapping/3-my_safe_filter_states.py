2724e09eceaf716353303743a4241fa89c33c27d
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-26 00:22:43 -0400

    Add script to filter states by name safely

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""Display values in the states table based on the given state name"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Get the state name from the argument
+    state_name = argv[4]
+
+    try:
+        # Prepare the SQL query using parameterized query
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+        # Execute the query with parameterized input
+        cursor.execute(query, (state_name,))
+
+        # Fetch and print the results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error: {}".format(e))
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
