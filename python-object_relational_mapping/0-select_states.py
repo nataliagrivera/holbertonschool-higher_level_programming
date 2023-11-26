29437d348b3c51a686cfbd810594453348fb856e
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-25 22:08:17 -0400

    Refactor MySQL query to select states starting with 'N'

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,24 +5,27 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    u_name = argv[1]
+    psw = argv[2]
+    base = argv[3]
 
-    """Connect to a MySQL server."""
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                        passwd=argv[2], db=argv[3])
+    # Connecting to MySQL database
+    db = MySQLdb.connect(host="localhost", user=u_name,
+                        passwd=psw, db=base, port=3306)
 
-    """Create a cursor object."""
+    # Creating cursor object
     cur = db.cursor()
 
-    """Execute the query."""
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Executing MySql Query
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
 
-    """Print the results of the query."""
-    for row in cur.fetchall():
-        print(row)
+    # Obtaining Query Result & prints the result in rows
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
 
-    """Close all cursors."""
+    # Clean Up
     cur.close()
-
-    """Close all databases."""
     db.close()
