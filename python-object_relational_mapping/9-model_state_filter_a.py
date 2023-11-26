72f4b44cf78fbed1a5d4d01f78a79b306a1eae3a
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-26 13:13:06 -0400

    Refactor query to improve readability

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    query = session.query(State).filter(State.name.like('%a%')) \
+                            .order_by(State.id)
 
     for state in query.all():
         print(f"{state.id}: {state.name}")
