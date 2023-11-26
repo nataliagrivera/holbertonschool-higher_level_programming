668d6ae2e483fe7586209d4b31d11a20469d9167
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-26 13:15:14 -0400

    Add script to retrieve state ID by name

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,18 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                        format(username, password, db_name), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.name == state_name).order_by(State.id).first()
+
+    if query:
+        print(query.id)
+    else:
+        print("Not found")
